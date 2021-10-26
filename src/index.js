b66304c8a67b326932f38b8bec6ec9d25c808409
Bhupinder Singh <bhupindersingh9443@gmail.com>
2021-10-26 13:10:01 +0530

    Added source code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import Prototype1 from "../src/JavaScript Practice/Prototype-Inheritance";
-//import Practice from "../src/JavaScript Practice/JavaScript";
-//import Practice2 from "../src/JavaScript Practice/w3Schools";
-//import PracticeArrays from "../src/JavaScript Practice/JavaScript Arrays";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import App from "./blog/components/App";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reducers from "./blog/reducers";
-
-import thunk from "redux-thunk";
-
-const store = createStore(reducers, applyMiddleware(thunk));
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
-
-// ReactDOM.render(<Prototype1 />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
