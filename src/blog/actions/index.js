9b36dc3f82d8b42f027af4514db93b1af4878aae
Bhupinder Singh <bhupindersingh9443@gmail.com>
2021-10-26 11:56:32 +0530

    Removed practice material

--- src/blog/actions/index.js
+++ src/blog/actions/index.js
@@ -5,20 +5,6 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// export const fetchUser = (id) => async (dispatch) => {
-//   const response = await jsonplaceholder.get(`/users/${id}`);
-//   dispatch({ type: "FETCH_USER", payload: response.data });
-// };
-
-// export const fetchUser = (id) => async (dispatch) => {
-//   const response = await jsonplaceholder.get(`/users/${id}`);
-
-//   dispatch({
-//     type: "FETCH_USER",
-//     payload: response.data,
-//   });
-// };
-
 export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonplaceholder.get(`users/${id}`);
   dispatch({
