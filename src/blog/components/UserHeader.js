875792c4757d7416875a9a734a0b47546e651924
Bhupinder Singh <bhupindersingh9443@gmail.com>
2021-10-26 11:51:52 +0530

    Removed comments

--- src/blog/components/UserHeader.js
+++ src/blog/components/UserHeader.js
@@ -27,29 +27,3 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
 
-// import React from "react";
-// import { fetchUser } from "../actions";
-// import { connect } from "react-redux";
-
-// class UserHeader extends React.Component {
-//   componentDidMount() {
-//     this.props.fetchUser(this.props.userId);
-//   }
-
-//   render() {
-//     const user = this.props.users.find((user) => user.id === this.props.userId);
-
-//     if (!user) {
-//       return null;
-//     }
-//     return <div> {user.name}</div>;
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     users: state.users,
-//   };
-// };
-
-// export default connect(mapStateToProps, { fetchUser })(UserHeader);
--- src/blog/reducers/usersReducer.js
+++ src/blog/reducers/usersReducer.js
@@ -7,21 +7,3 @@ export const usersReducer = (state = [], action) => {
   }
 };
 
-// export const usersReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case "FETCH_USERS":
-//       return [...state, action.payload];
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export const usersReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case "FETCH_USER":
-//       return [...state, action.payload];
-//     default:
-//       return state;
-//   }
-// };
